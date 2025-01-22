484a12a1230fac055a0714e7cfd7070da52040df
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-22 11:14:02 +0530

    Update 852.java

--- 852.java
+++ 852.java
@@ -1,1 +1,16 @@
 https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int max = arr[0];
+        int index=0 ;
+        for( int i = 0; i<arr.length;i++){
+            if(arr[i]>max){
+                max= arr[i];
+                index = i;
+            }
+
+        }
+        return index;
+    }
+}
