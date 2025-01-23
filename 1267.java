2d5ad04968a52693557a4578faf3d17d397dc321
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-23 21:41:36 +0530

    Create 1267.java

---
+++ 1267.java
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/count-servers-that-communicate/submissions/1518129495/?envType=daily-question&envId=2025-01-23
+
+class Solution {
+    public int countServers(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+        
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+        
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1)) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
