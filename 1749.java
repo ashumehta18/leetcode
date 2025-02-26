30611886a4a7f3a2a197579e645fe1fb26abac0c
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-02-26 19:51:17 +0530

    Create 1749.java

---
+++ 1749.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/?envType=daily-question&envId=2025-02-26
+
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int maxSum = 0, minSum = 0;
+        int currentMax = 0, currentMin = 0;
+        
+        for (int num : nums) {
+            currentMax = Math.max(0, currentMax + num);
+            maxSum = Math.max(maxSum, currentMax);
+            currentMin = Math.min(0, currentMin + num);
+            minSum = Math.min(minSum, currentMin);
+        }
+        
+        return Math.max(maxSum, Math.abs(minSum));
+    }
+}
