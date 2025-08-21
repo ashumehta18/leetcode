ef2575bda132669c0ff7b8eecd4bd15fdf4534dd
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-08-21 18:29:57 +0530

    time complexity = 0(log N)

---
+++ 0342.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/power-of-four/description/
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n == 0){
+            return false;
+        }
+
+        while(n%4 == 0){
+            n = n/4;
+        }
+        return n == 1;
+    }
+}
