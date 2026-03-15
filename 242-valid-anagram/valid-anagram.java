feede93cf88138bc9b3f35f0a1c1cfc1cbec381b
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-03-15 16:30:47 +0530

    Time: 20 ms (9.45%) | Memory: 47 MB (7.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,28 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        if(s.length() == 0 && t.length() == 0){
-            return true;
-        }
         HashMap<Character,Integer> map = new HashMap<>();
-        for(int i = 0 ; i<s.length();i++){
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0) +1);
+        if(s.length() != t.length()) return false;
+
+        for(int i = 0 ;i<s.length();i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
         }
-        for(int j = 0 ;j<t.length();j++){
-            if(map.containsKey(t.charAt(j))){
-                map.put(t.charAt(j), map.get(t.charAt(j)) - 1);
-                if (map.get(t.charAt(j)) < 0){
+        for(int i = 0 ;i<t.length();i++){
+            if(!map.containsKey(t.charAt(i))){
+                return false;
+            }else{
+                map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+                if (map.get(t.charAt(i)) < 0){
                     return false;
                 }
-                //count -- krna h of a particular char
-                //if count less than 0 jae toh bhi false return 
-            }else{
-                return false;
             }
         }
-
         return true;
     }
 }
