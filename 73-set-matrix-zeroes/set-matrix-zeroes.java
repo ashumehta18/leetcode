4374513f3060ef7083a85ab9fc063896bae22fa2
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-03-17 23:08:54 +0530

    Time: 1 ms (65.91%) | Memory: 47.8 MB (7.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,29 +1,27 @@
-import java.util.*;
-
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
+        boolean col = false;
+        boolean row = false;
+        for(int i = 0 ;i<matrix.length;i++) if(matrix[i][0] == 0) col = true;
+        for(int j = 0 ;j<matrix[0].length;j++) if(matrix[0][j] == 0) row = true;
 
-        boolean col0 = false;
-
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] == 0) col0 = true;
-
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
+        for(int i = 1;i<matrix.length;i++){
+            for(int j = 1 ;j<matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0]=0 ;
                     matrix[0][j] = 0;
                 }
             }
         }
+        for(int i = 1 ;i<matrix.length;i++){
+            for(int j = 1 ;j<matrix[0].length;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 1; j--) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                    matrix[i][j] = 0;
             }
-            if (col0) matrix[i][0] = 0;
         }
+        if(col) for(int i = 0 ;i<matrix.length;i++) matrix[i][0] = 0;
+        if(row) for(int j = 0 ;j<matrix[0].length;j++) matrix[0][j] = 0;
+        
+
     }
 }
