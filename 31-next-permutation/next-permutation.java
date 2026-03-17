89568bc552ccfcde68c8c746c110a31f33ce75a7
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-03-17 23:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (45.81%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,39 +1,32 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        // find break point
-        // swap arr[i]<arr[i+1]
-        // reverse if no greater found
-        int n = nums.length;
-        int idx = -1;
-        for(int i = n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                idx = i;
-                break;
+        int i = nums.length -2;
+        while(i>=0 && nums[i]>= nums[i+1]){
+            i--;
+        } // if not found decrement by 1 i each time 
+        if(i>=0){
+            int j = nums.length -1;
+            while(nums[j]<=nums[i]){ //check for the other no. with which swap need to be encountred
+                j--;
             }
+            swap(nums,i,j); // swap both no.s 
         }
-            if(idx == -1){
-                reverse(nums,0,n-1);
-                return;
-            }
-            for(int i = n-1;i>idx;i--){
-                if(nums[i]>nums[idx]){
-                    swap(nums,i,idx);
-                    break;
-                }
-            }
-            reverse(nums,idx+1,n-1);
         
+        reverse(nums,i+1,nums.length -1); // reverse as back from is sorted in asc to desc
     }
-    public void swap(int[] nums,int start,int end){
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
+    public void swap(int[] arr,int left, int right){
+        if(left == right) return;
+        arr[left] = arr[left]^arr[right];
+        arr[right] = arr[right]^arr[left];
+         arr[left] = arr[left]^arr[right];
+
     }
-    public void reverse(int[] nums, int start, int end){
-        while(start<end){
-            swap(nums,start,end);
-            start++;
-            end--;
-        }
+    public void reverse(int[] arr,int left,int right){
+    //right = arr.length -1;
+    while(left<right){
+        swap(arr,left,right);
+         left++;
+         right--;
+    }
     }
 }
