d0f36a004cb337c1b4ed71047821e46a9ec6c17f
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-03-18 13:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (64.52%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -3,30 +3,25 @@ class Solution {
         int i = nums.length -2;
         while(i>=0 && nums[i]>= nums[i+1]){
             i--;
-        } // if not found decrement by 1 i each time 
+        }
         if(i>=0){
             int j = nums.length -1;
-            while(nums[j]<=nums[i]){ //check for the other no. with which swap need to be encountred
-                j--;
-            }
-            swap(nums,i,j); // swap both no.s 
+            while(nums[j]<=nums[i]){
+           j--;
+            } swap(nums,i,j);
         }
-        
-        reverse(nums,i+1,nums.length -1); // reverse as back from is sorted in asc to desc
+        reverse(nums,i+1,nums.length-1);
     }
-    public void swap(int[] arr,int left, int right){
-        if(left == right) return;
-        arr[left] = arr[left]^arr[right];
-        arr[right] = arr[right]^arr[left];
-         arr[left] = arr[left]^arr[right];
-
+    public void swap(int[] nums,int left,int right){
+        nums[left] ^= nums[right]; 
+        nums[right] ^= nums[left] ; 
+         nums[left] ^= nums[right]; 
     }
-    public void reverse(int[] arr,int left,int right){
-    //right = arr.length -1;
-    while(left<right){
-        swap(arr,left,right);
-         left++;
-         right--;
-    }
+    public void reverse(int[] nums,int left,int right){
+        while(left<right){
+            swap(nums,left,right);
+            left++;
+            right--;
+        }
     }
 }
