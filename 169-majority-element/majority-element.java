873f01a5ca02b76754c8ec1d59845710f159bc69
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-03-18 15:13:21 +0530

    Time: 1 ms (99.82%) | Memory: 55.7 MB (44.79%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int max = nums[0];
+        int count = 0;
+        for(int i = 0 ;i<nums.length;i++){
+            if(count == 0){
+                max = nums[i];
+            }
+            if(nums[i] == max){
+                count++;
+            }else{
+                count--;
+                
+            }
+            
+        }
+        return max;
+    }
+}
