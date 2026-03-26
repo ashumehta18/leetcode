ce4f6cff049d8eeb40b8d7023b5a48c41554a425
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-03-26 16:20:36 +0530

    Time: 16 ms (21.96%) | Memory: 52.6 MB (59.26%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -5,10 +5,9 @@ class Solution {
 
         for(int i = 0 ;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        
-
-       // for(int i = 0 ;i<nums.length ;i++){
-            if(map.get(nums[i]) > (nums.length/3)){
+        }
+        for(int i = 0 ;i<nums.length;i++){
+            if(map.get(nums[i])>nums.length/3){
                 list.add(nums[i]);
             }
         }
