322debfc5c081987c5a63e1c8050c923db44f4e5
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-03-30 13:11:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (46.13%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,22 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int [n+1];
+       // int[] dp = new int [n+1];
        // int ans = 0;
         if(n<=2) return n;
-        dp[1] =1;
-        dp[2] =2;
+        int c; //just extra varriables we take no extra space
+        int a  =1;
+        int b  =2;
         for(int i = 3 ;i<=n;i++){
            //if(dp[i] != 0) return dp[i];
-           dp[i] = dp[i-1]+ dp[i-2];
+        //    dp[i] = dp[i-1]+ dp[i-2];
            //dp[i] = ans;
+           c=a+b;
+           a=b;
+           b=c;
         }
         
-        return  dp[n];
+        return  b;
     }
+    //practical implementation of fibonnaic no. 
     
 }
