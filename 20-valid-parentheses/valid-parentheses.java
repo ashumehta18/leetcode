5faed8e65a45358a3a10510eaf2932acfb6f7fea
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-04-17 10:21:22 +0530

    Time: 4 ms (41.18%) | Memory: 43.8 MB (6.13%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,19 +3,13 @@ class Solution {
         Stack<Character> st = new Stack<>();
 
         for(int i = 0 ;i<s.length();i++){
-            if(s.charAt(i) == '[' || s.charAt(i) == '(' || s.charAt(i) == '{' ){
+            if(s.charAt(i) == '('|| s.charAt(i) == '{'|| s.charAt(i) == '['){
                 st.push(s.charAt(i));
             }else{
                 if(st.isEmpty()) return false;
-                if(s.charAt(i) == ']'&& st.peek() !='[' ){
-                    return false;
-                }
-                if(s.charAt(i) == '}'&& st.peek() !='{' ){
-                    return false;
-                }
-                if(s.charAt(i) == ')'&& st.peek() !='(' ){
-                    return false;
-                }
+                if(s.charAt(i) =='}' && st.peek() != '{') return false;
+                if(s.charAt(i) ==')' && st.peek() != '(') return false;
+                if(s.charAt(i) ==']' && st.peek() != '[') return false;
                 st.pop();
             }
         }
