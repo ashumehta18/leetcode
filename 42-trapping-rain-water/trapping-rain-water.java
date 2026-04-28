aecee73b9a57500fdf94e8c670cfcc88ee6db2ab
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-04-28 17:40:02 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (81.49%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -3,20 +3,20 @@ class Solution {
         int max = 0 , lmax = 0, rmax = 0 , l = 0;
         int r= arr.length-1;
 
-        while(l<r){
-            if(arr[l]<=arr[r]){
-                if(lmax>=arr[l]){
+        while(l<r){ //left part less thna right 
+
+            if(arr[l]<=arr[r]){ //values left are less
+                if(lmax>=arr[l]){ //left max is greater than curr
                     max += lmax - arr[l];
-                    
                 }else{
                     lmax = arr[l];
                 }
                 l++;
-            }else{
+            }else{  //values at right are less
                 if(rmax>arr[r]){
                     max += rmax-arr[r];
                     
-                }else{
+                }else{ 
                     rmax = arr[r];
                 }
                 r--;
