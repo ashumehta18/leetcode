88febb07a1a4dbaf772de9a0ad5df467515db1ff
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-04-28 17:37:03 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (69.46%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,29 +1,27 @@
 class Solution {
-    public int trap(int[] height) {
-        //max array - left
-        //max array - right 
-        //height = min(left, max) -height at i;
-        //add all the height will sum upto result
-        int res = 0;
-        int n = height.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
-        int max = 0;
-        for(int i = 0 ;i<n ;i++){
-            max = Math.max(max,height[i]);
-            left[i] = max;
-        }
-        int maxr = 0;
-        for(int i = n-1;i>=0;i--){
-            maxr= Math.max(maxr,height[i]);
-            right[i] = maxr;
-        }
-        for(int i = 0 ;i<n ;i++){
-            height[i] = Math.min(left[i],right[i]) -height[i];
-            res+=height[i];
-        }
+    public int trap(int[] arr) {
+        int max = 0 , lmax = 0, rmax = 0 , l = 0;
+        int r= arr.length-1;
 
-        return res ;
-
+        while(l<r){
+            if(arr[l]<=arr[r]){
+                if(lmax>=arr[l]){
+                    max += lmax - arr[l];
+                    
+                }else{
+                    lmax = arr[l];
+                }
+                l++;
+            }else{
+                if(rmax>arr[r]){
+                    max += rmax-arr[r];
+                    
+                }else{
+                    rmax = arr[r];
+                }
+                r--;
+            }
+        }
+        return max;
     }
 }
