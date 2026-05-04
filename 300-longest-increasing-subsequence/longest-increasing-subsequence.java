30a70ab756b3b11f1c4ff8ca8a52c9d65ab9a91e
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-05-04 18:44:58 +0530

    Time: 53 ms (26.97%) | Memory: 46.2 MB (54.63%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,47 +1,19 @@
 class Solution {
-    // public int lengthOfLIS(int[] nums) {
-    //     int start = 0;
-    //     int end = nums.length;
-    //     int[] ans = new int[];
+    public int lengthOfLIS(int[] arr) {
+        int max = 1;
+        int n = arr.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,1);
 
-    //     ans[0] = nums[0];
-
-    //     for(int i = 1;i<end;i++){
-    //         if(nums[i]>nums[0]){
-    //             //add element at last position of ans array
-    //         }
-    //         else if(nums[i]<nums[0]){
-    //             //add element at start
-    //         }else{
-    //             //if in btw lies then using lower bound add kro 
-    //         }
-
-    //     }
-    //     return ans.length;
-    // }
-
-    public int lengthOfLIS(int[] nums) {
-    if (nums == null || nums.length == 0) return 0;
-
-    int[] ans = new int[nums.length];
-    int size = 0; // Tracks the current length of the LIS found so far
-
-    for (int x : nums) {
-        int i = 0, j = size;
-        
-        // Binary Search to find the lower bound (insertion point)
-        while (i != j) {
-            int mid = (i + j) / 2;
-            if (ans[mid] < x)
-                i = mid + 1;
-            else
-                j = mid;
+        for(int i = 1;i<n;i++){
+            for(int j = 0 ;j<i;j++){
+                if(arr[j]<arr[i]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+                max = Math.max(max, dp[i]);
+            }
         }
+        return max;
         
-        ans[i] = x; // Replace or extend
-        if (i == size) size++; // If we added to the end, increase size
     }
-    
-    return size;
-}
 }
