4a9e654a22afde0e082b190fdd46e068b2489c8c
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-05-09 23:39:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (15.31%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,27 +1,31 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i = nums.length -2;
-        while(i>=0 && nums[i]>= nums[i+1]){
+        int i = nums.length-2;
+        //pivot point find
+        while(i>= 0 && nums[i]>=nums[i+1]){
             i--;
-        }
-        if(i>=0){
-            int j = nums.length -1;
-            while(nums[j]<=nums[i]){
-           j--;
-            } swap(nums,i,j);
+        }if(i>=0){
+            int j = nums.length-1;
+            while(nums[j]<= nums[i]){
+                j--;
+            }swap(nums,i,j);
         }
         reverse(nums,i+1,nums.length-1);
+        
     }
-    public void swap(int[] nums,int left,int right){
-        nums[left] ^= nums[right]; 
-        nums[right] ^= nums[left] ; 
-         nums[left] ^= nums[right]; 
+    public void swap(int[] nums,int start,int end){
+         nums[start] ^= nums[end];
+            nums[end] ^= nums[start];
+            nums[start] ^= nums[end];
+        
     }
-    public void reverse(int[] nums,int left,int right){
-        while(left<right){
-            swap(nums,left,right);
-            left++;
-            right--;
+    public void reverse(int[] nums,int start,int end){
+        while(start<end){
+            nums[start] ^= nums[end];
+            nums[end] ^= nums[start];
+            nums[start] ^= nums[end];
+            start++;
+            end--;
         }
     }
 }
