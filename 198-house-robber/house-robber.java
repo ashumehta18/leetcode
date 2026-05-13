5a766e54668ceac95d4a527a50b70745fcf09e99
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-05-13 15:04:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (86.93%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,29 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-    int n = nums.length;
-    int[] dp = new int[n+1];
-    
-    if(n == 0) return 0;
-    if(n==1) return nums[0];
+        int n = nums.length;
+        int[] dp = new int[n];
 
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0],nums[1]);
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-    for(int i = 2 ;i<n;i++){
-        dp[i] = Math.max(dp[i-1],nums[i]+dp[i-2]);
-    }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
 
-    return dp[n-1];
-
+        for(int i = 2 ;i<n;i++){
+            if(dp[i] != 0) return dp[i];
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
+        
     }
 }
-
-//find the length
-// create dp array
-// check for n = 0 =>0
-//n==1 => ist index element
-// base case first elemnt of dp = first elememt of nums[0];
-//..2nd ele max from 1 and 2
-
-//itterate to find dp[i] value 
-//return dp[n-1]
